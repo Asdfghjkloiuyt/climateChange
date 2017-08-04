2f86813a8b3c327ac2b21bb1ddcb06bd143d6716
monserathflores <monserathflowers@gmail.com>
2017-08-04 09:01:43 -0700

    xhanges
    changes

--- Final Project/projectMonse/graphs0.js
+++ Final Project/projectMonse/graphs0.js
@@ -1,7 +1,6 @@
 
-Plotly.d3.csv('projectMonse/outFile_beijing_weather2.csv', function(rows){
-  console.log(window.location.pathname);  
-  var trace = {
+Plotly.d3.csv('../projectMonse/outFile_beijing_weather2.csv', function(rows){
+    var trace = {
       type: 'scatter',                    // set the chart type
       mode: 'lines',                      // connect points with lines
       x: rows.map(function(row){          // set the x-data
